180d4d01701d23069ce313c249ee85e55d0f154b
yousof-ali <yousofali293@gmail.com>
2024-05-02 11:48:03 +0600

    calculate done

--- scripts/area-calc.js
+++ scripts/area-calc.js
@@ -4,10 +4,20 @@ function triangleArea(){
 
     // get height
     const heightField  = getInputValue("th");
+    const validate = inputVlidate(basefield,heightField);
 
-    const result = 0.5 * basefield * heightField;
+    if(validate){
+        const result = 0.5 * basefield * heightField;
 
-    setvalue("tri-result",result);
+        setvalue("tri-result",result);    
+    }
+    else{
+        alert("input will be number");
+        setvalue("tri-result",0);
+        setDefaultInput("tb","th");
+    }
+
+   
 };
 
 function rectangleArea(){
@@ -16,9 +26,19 @@ function rectangleArea(){
 
     const rectlength = getInputValue("rectL");
 
-    const result = rectwidth * rectlength;
+    const validate = inputVlidate(rectwidth,rectlength);
+    if (validate){
+        const result = rectwidth * rectlength;
 
-    setvalue("rect-result",result);
+        setvalue("rect-result",result);
+    }
+    else{
+        alert("input will be a number")
+        setvalue("rect-result",0);
+        setDefaultInput("rectW","rectL");
+    }
+
+    
 
 }
 
@@ -27,10 +47,78 @@ function paraArea(){
     const paraBase = getInputValue("paraB");
     // get height 
     const paraHeight = getInputValue("paraH");
-    
+    const validate = inputVlidate(paraBase,paraHeight);
     // area 
-    const result  = paraBase * paraHeight;
+    if(validate){
+        const result  = paraBase * paraHeight;
 
-    // set 
-    setvalue("para-result",result);
+        // set 
+        setvalue("para-result",result);
+    }
+    else{
+        alert("input will be number");
+        setvalue("para-result",0);
+        setDefaultInput("paraB","paraH");
+    }
+    
+}
+
+
+function rhomArea(){
+    const rhomD1 = getInputValue("rhomD1");
+    const rhomD2 = getInputValue("rhomD2");
+    
+    const validate = inputVlidate(rhomD1,rhomD2);
+    
+    if(validate){
+        const result = 0.5 * rhomD1 * rhomD2;
+
+    setvalue("rhom-result",result);
+    }
+    else{
+        alert("input will be a number");
+        setvalue("rhom-result",0);
+        setDefaultInput("rhomD1","rhomD2");
+
+    }
+}
+
+function pentaArea(){
+    const pentaP = getInputValue("pentaP");
+    const pentaBase = getInputValue("pentaBase");
+
+    const validate =  inputVlidate(pentaP,pentaBase);
+
+    if(validate){
+        const result = 0.5 * pentaP * pentaBase;
+
+        setvalue("penta-result",result);
+    }
+    else{
+        alert("input will be a number")
+        setvalue("penta-result",0);
+        setDefaultInput("pentaP","pentaBase");
+    }
+}
+
+
+function elliArea(){
+    const elliA = getInputValue("elliA");
+    const elliB = getInputValue("elliB");
+
+    const validate = inputVlidate(elliA,elliB);
+
+    if (validate){
+        const result = 3.14 * elliA * elliB;
+        const resultTwo = result.toFixed(2);
+
+        setvalue("elli-result",resultTwo);
+    }
+    else{
+        alert("input will be a number");
+        setvalue("elli-result",0);
+        setDefaultInput("elliA","elliB");
+    }
+
+    
 }
--- scripts/input.js
+++ scripts/input.js
@@ -11,3 +11,25 @@ function setvalue(elementId, val){
     const result = document.getElementById(elementId);
     result.innerText = val;
 }
+
+
+
+// validate 
+function inputVlidate(value1,value2){
+    if (isNaN(value1) || isNaN(value2)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+
+// setdefault input value 
+
+function setDefaultInput(elementId1, elementId2){
+    const elements = document.getElementById(elementId1);
+    elements.value = '';
+    const elements2 = document.getElementById(elementId2);
+    elements2.value = '';
+}
